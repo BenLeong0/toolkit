100fb03ddca0cdee0a315d75d8016789eb4a6cf2
benleong0 <benleong@myurbanjungle.com>
2024-03-16 13:50:58 +0000

    landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<div
+    class="bg-gradient-to-b from-teal-800 to-teal-400 h-screen flex items-center flex-col pt-40"
+>
+    <h1 class="text-8xl text-white font-semibold mb-4">Welcome to SvelteKit</h1>
+    <p class="text-white text-xl italic font-thin">
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+</div>
